33fee4e2bd96a106ccaa3ebb091202ca8ed6a9b2
Pinapelz <donaldshan1@outlook.com>
2023-06-17 17:15:31 -0700

    feat: py commit message generation

--- main.py
+++ main.py
@@ -1,42 +1,73 @@
 import data_converter
 from yt_downloader import YouTubeDownloader
 import subprocess
-import os
-import json
 import file_util
+from input_enums import Options
+from datetime import datetime
 
 CONFIG = file_util.read_config("C:\\Users\\donal\\OneDrive\\Desktop\Covers-Worker\\config.ini")
 
 def rclone_to_cloud():
-    subprocess.run(f'{CONFIG.get("rclone_path")} -P copy "{CONFIG.get("path","download_output_path")}" "{CONFIG.get("path","rclone_cloud_target")}"')
+    subprocess.run(f'{CONFIG.get("path","rclone_path")} -P copy "{CONFIG.get("path","download_output_path")}" "{CONFIG.get("path","rclone_cloud_target")}"')
 
-def clear_output_folder(file_path: str = CONFIG.get("path","download_output_path")):
-    for file in os.listdir(file_path):
-        os.remove(f"{file_path}\\{file}")
+def additional_commands_input():
+    print("\n\n")
+    print("Finished Task... What would you like to do next?")
+    print("1. Sort Videos ndjson")
+    print("2. Commit to remote")
+    print("3. Search for video in ndjson (must be sorted)")
+    print("4. Validate ndjson")
+    print("5. Download and Upload again")
+    print("6. Exit")
+    print("\n\n")
+    try:
+        return int(input())
+    except ValueError:
+        print("Invalid Input")
+        return additional_commands_input()
 
-def verify_ndjson():
-    with open("CoversArchive/covers.ndjson", "r", encoding="utf-8") as f:
-        for line in f:
-            try:
-                x = json.loads(line)
-                if x["video_id"] == "lvDNdCBcKJE":
-                    print(x)
-            except json.JSONDecodeError:
-                print("Invalid JSON line: ", line)
-                return False
 
-def main():
+def download_and_upload():
     print("Clear output folder? (y/n)")
-    clear_output_folder() if input() == "y" else print("Skipping...")
+    file_util.clear_output_folder(CONFIG.get("path","download_output_path")) if input() == "y" else print("Skipping...")
     print("Download Videos? (y/n)")
     downloader = YouTubeDownloader("download_list.txt")
     if input() == "y":
         downloader.download_urls()
         data_converter.convert_all_mkv_to_webm(CONFIG.get("path","download_output_path"))
-    print("Download Complete... Ready to generate csv data (y/n)")
-    data_converter.generate_json_data("output_video", ".webm") if input() == "y" else print("Skipping...")
+    print("Download Complete... Ready to generate ndjson data (y/n)")
+    data_converter.generate_json_data(video_input_dir = "output_video",file_type = ".webm", ndjson_path= CONFIG.get("path", "ndjson_path")) if input() == "y" else print("Skipping...")
     print("JSON Data Generated... Upload to Cloud? (y/n)")
     rclone_to_cloud() if input() == "y" else print("Skipping...")
+
+def _make_commit_message():
+    commit_message = f"Generation at {datetime.now()}\n\n"
+    for video in data_converter.get_all_files_in_directory(CONFIG.get("path","download_output_path"), file_type = "webm"):
+        commit_message += f"{video}\n"
+    return commit_message
+
+def main():
+    nd_json_reader = file_util.NDJsonReader(CONFIG.get("path","ndjson_path"))
+    download_and_upload()
+    cmd = additional_commands_input()
+    while cmd != Options.EXIT.value:
+        match cmd:
+            case Options.SORT_VIDEOS.value:
+                nd_json_reader.sort_ndjson("video_id")
+            case Options.COMMIT_TO_REMOTE.value:
+                data_converter.commit_and_push(CONFIG.get("path", "git_repo_path"), _make_commit_message())
+            case Options.SEARCH_FOR_VIDEO.value:
+                print("Enter video id to search for: ")
+                video_id = input()
+                nd_json_reader.search_for_video_id(video_id)
+            case Options.VALIDATE_NDJSON.value:
+                nd_json_reader.validate_ndjson()
+            case Options.DOWNLOAD_AND_UPLOAD.value:
+                download_and_upload()
+            case _:
+                print("Invalid Input")
+        cmd = additional_commands_input()
+
 
 
 
