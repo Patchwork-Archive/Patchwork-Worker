48d0ece112faeaf688633a64de9c7f8e8250425b
Pinapelz <donaldshan1@outlook.com>
2023-07-11 16:44:44 -0700

    fix: Send offfline heartbeat also if sleeping

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ import requests
 import time
 import configparser
 
+ERROR_WAIT_TIME = 500 # seconds
+COOLDOWN_WAIT_TIME = 250 # seconds
+
 def read_config(file_path: str):
     """
     Reads a config file and returns a dictionary of the config
@@ -25,11 +28,22 @@ def send_heartbeat(status: str):
     headers = {'X-AUTHENTICATION': password}
     requests.post(f"{base_url}/api/worker/heartbeat", headers=headers, data={"status": status, "name": name})
 
+
+def wait_and_check_for_quit(seconds: int):
+    try:
+        for _ in range(seconds):
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Keyboard interrupt detected. Sending offline heartbeat...")
+        send_heartbeat("Offline")
+        exit()
+
+
 def main():
     config = read_config("config.ini")
     base_url = config.get("queue", "base_url")
     password = config.get("queue", "worker_password")
-    send_heartbeat("Online")
+    send_heartbeat("Offline")
     try:
         while True:
             headers = {'X-AUTHENTICATION': password}
@@ -40,11 +54,12 @@ def main():
                 worker.execute_server_worker(next_video.text)
             elif next_video.status_code == 401:
                 print("Invalid credentials. The password may be incorrect")
-                time.sleep(500)
+                send_heartbeat("Invalid credentials. The password may be incorrect")
+                wait_and_check_for_quit(ERROR_WAIT_TIME)
             else:
                 print("No videos to archive at this time. Cooling down...")
                 send_heartbeat("Idle. Waiting for work...")
-                time.sleep(250)
+                wait_and_check_for_quit(COOLDOWN_WAIT_TIME)
     except Exception as e:
         if str(e) == "KeyboardInterrupt":
             print("Keyboard interrupt detected. Sending offline heartbeat...")
